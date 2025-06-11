748daf2c2f27ca884e44c5a852a85025b1eaed21
João Marcos <joaomarcos.vidcar@gmail.com>
2025-06-10 23:53:23 -0300

    Diets Routes

--- requisites.md
+++ requisites.md
@@ -3,14 +3,14 @@
 <!--  -->
 
 -- [x] Deve ser possível criar um usuário
--- [] Deve ser possível registrar uma refeição feita, com as seguintes informações:
+-- [x] Deve ser possível registrar uma refeição feita, com as seguintes informações:
 ====Nome
 ====Descrição
 ====Data e Hora
 ====Está dentro ou não da dieta
--- [] Deve ser possível editar uma dieta
--- [] Deve ser possível apagar uma refeição
--- [] Deve ser possível listar todas as refeições de um usuário
+-- [x] Deve ser possível editar uma dieta
+-- [x] Deve ser possível apagar uma refeição
+-- [x] Deve ser possível listar todas as refeições de um usuário
 -- [] Deve ser possível visualizar uma única refeição
 -- [] Deve ser possível recuperar as métricas de um usuário:
 ===Total de refeições registradas
@@ -22,5 +22,5 @@
 <!--  -->
 <!--  -->
 
--- [] O usuário só pode visualizar, editar e apagar as refeições que ele criou
--- [] Deve ser possível identificar o usuário entre as requisições
+-- [x] O usuário só pode visualizar, editar e apagar as refeições que ele criou
+-- [x] Deve ser possível identificar o usuário entre as requisições
--- src/routes/dietsRouter.ts
+++ src/routes/dietsRouter.ts
@@ -1,21 +1,29 @@
 import { FastifyInstance } from "fastify";
 import {knex} from "../../db/database"
-import {z} from "zod"
+import {string, z} from "zod"
 import { userIdValidation } from "../middlewares/userIdValidation";
 import { randomUUID } from "crypto";
 
 export async function dietsRouter(app: FastifyInstance) {
+  /* User Validation Hook */
+  /*  */
 
-  /* GET ALL DIETS ROUTE */
+  app.addHook('onRequest', userIdValidation)
+
+  /* Get all DIETS Route */
   /*  */
 
   app.get('/', async (request, reply) => {
+    const data = await knex('diets').where('userID', request.userID).select('*')
+
+    return reply.status(200).send({ data })
+
   })
 
   /* Create a new DIET Route */
   /*  */
 
-  app.post('/', {preHandler: userIdValidation}, async (request, reply) => {
+  app.post('/', async (request, reply) => {
 
     /* Validate Request Schema */
 
@@ -47,4 +55,66 @@ export async function dietsRouter(app: FastifyInstance) {
     return reply.status(201).send()
   })
 
+  /* Update a DIET Route */
+  /*  */
+
+  app.put('/:id', async (request, reply) => {
+    
+    /* Validate Request Body */
+    
+    const requestBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      diet: z.boolean()
+    })
+
+    /* Validate Request Param ID */
+
+    const requestParamSchema = z.object({
+      id: string().uuid()
+    })
+
+    /* Get variables */
+
+    const {name, description, diet} = requestBodySchema.parse(request.body)
+    const {userID} = request
+    const {id} = requestParamSchema.parse(request.params)
+
+    /* Update Diet */
+
+    await knex('diets').where({
+      id,
+      userID
+    }).update({
+      name,
+      description,
+      diet
+    })
+
+    return reply.status(201).send()
+  })
+
+  /* Delete a DIET Route */
+  /*  */
+
+  app.delete('/:id', async (request, reply) => {
+
+    /* Validate Request Params */
+
+    const requestParamsSchema = z.object({
+      id: z.string().uuid()
+    })
+
+    const {id} = requestParamsSchema.parse(request.params)
+
+    /* Delete Diet */
+
+    await knex('diets').where({
+      id,
+      userID: request.userID
+    })
+    .del()
+
+    return reply.status(201).send()
+  })
 }
