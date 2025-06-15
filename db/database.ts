cb5b36a31d8ff378db67a3dc81adb4b0f98099fc
João Marcos <joaomarcos.vidcar@gmail.com>
2025-06-15 18:12:16 -0300

    Database Postgress Setup

--- db/database.ts
+++ db/database.ts
@@ -1,11 +1,19 @@
 import {knex as knexSetup} from "knex"
 import { env } from "../src/env/setup"
 
+let connection = {}
+
+if (env.DATABASE_CLIENT === "sqlite3") {
+  connection = {
+    filename: env.DATABASE_URL
+  }
+} else if (env.DATABASE_CLIENT === "pg") {
+  connection = env.DATABASE_URL
+}
+
 export const config: knexSetup.Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection: {
-    filename: env.DATABASE_URL
-  },
+  connection,
   useNullAsDefault: true,
   migrations: {
     directory: "./db/migrations",
