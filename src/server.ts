702dbe1a0aaf2be333dd0db7693b02de4b35066f
João Marcos <joaomarcos.vidcar@gmail.com>
2025-06-15 18:27:09 -0300

    Adding Host Variable to Server

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import { app } from "./app";
 import { env } from "./env/setup";
 
 app.listen({
-  port: env.PORT
+  port: env.PORT,
+  host: "0.0.0.0",
 }).then(() => {
   console.log('HTTP Server is running...')
   console.log('Enviroment: ' + env.NODE_ENV)
